533445c03957c04f041102e2c4fbff0520a4c3f2
chelotron <36937945+chelotron@users.noreply.github.com>
2018-03-29 00:13:00 +0000

    Create vector.h

---
+++ vector.h
@@ -0,0 +1,17 @@
+#pragma once
+class vector
+{
+private:
+	int vec[100], n;
+public:
+	//cnstc
+	vector(void);
+	//dstrc
+	~vector(void);
+	//metods
+	void charge(int vec[], int n);
+	void show(int vec[], int n);
+	void order(int vec[], int n);
+	
+};
+
